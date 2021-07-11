965cb929133365c952e29f281d83454d47fc856e
Rodionn <ro@expodat.com>
2021-07-11 17:54:25 +0300

    Верхнее меню

--- www/catalog/controller/common/header.php
+++ www/catalog/controller/common/header.php
@@ -71,7 +71,7 @@ class ControllerCommonHeader extends Controller {
 		$data['contact'] = $this->url->link('information/contact');
 		$data['telephone'] = $this->config->get('config_telephone');
 
-		$data['email'] = $this->config->get('config_email');//Добавил почту
+		$data['email'] = $this->config->get('config_email');//mail
 
 		$data['language'] = $this->load->controller('common/language');
 		$data['currency'] = $this->load->controller('common/currency');
